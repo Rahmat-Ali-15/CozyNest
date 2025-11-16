8218d403f440b3cb888bd3cebbf82d209fb199fb
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-16 19:23:36 +0530

    Modified code

--- JS/SignIn_Page.js
+++ JS/SignIn_Page.js
@@ -1,11 +1,21 @@
+const defaultEmail = "rahmat786@gmail.com"
+const defaultPass = "rahmat@786"
+
 const signUpForm = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let signUpEmail = document.querySelector("#email").value;
-    let signUpPass = document.querySelector("#password").value;
+  let signUpEmail = document.querySelector("#email").value;
+  let signUpPass = document.querySelector("#password").value;
 
-    if(signUpEmail.trim() === "" || signUpPass.trim() === ""){
-        alert("All fields are required!!!");
-        return false;
-    }
-}
+  if (signUpEmail.trim() === "" || signUpPass.trim() === "") {
+    alert("All fields are required!!!");
+    return false;
+  }
+
+  if ((signUpEmail === defaultEmail) && (signUpPass === defaultPass)) {
+    alert("Login Successfull!!!");
+  }
+  else{
+    alert("Invalid email or Password !!!")
+  }
+};
