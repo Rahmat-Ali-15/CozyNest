59a11cb6506eb78d3fa9f9c9c2889c210ad3b26f
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-19 21:10:18 +0530

    Modified cod

--- JS/SignIn_Page.js
+++ JS/SignIn_Page.js
@@ -1,4 +1,6 @@
-let api_user = "https://api-server-zecj.onrender.com/user";
+// let api_user = "https://api-server-zecj.onrender.com/user";
+
+let userDataStorage = JSON.parse(localStorage.getItem("userDetails")) || [];
 
 const signInForm = async (e) => {
   e.preventDefault();
@@ -11,31 +13,40 @@ const signInForm = async (e) => {
     return false;
   }
 
-  // Fetching the from the api(data base)
-  try {
-    let usersRes = await fetch(api_user);
-    let users = await usersRes.json();
-
-    // Here finding user with match email and password
-    let user = users.find(
-      (u) => u.email === signInEmail && u.pass === signInPass
-    );
-    if (user) {
-      window.location = "/index.html";
-      // document.querySelector("#user_lastName").innerText = user.lName;
-    } else {
-      alert("Invalid email or password ❌");
-    }
-  } catch (error) {
-    console.log("Server Error", error);
-    alert("Something went wrong! Please try again.");
+  let user = userDataStorage.find((el) => el.email === signInEmail && el.pass=== signInPass);
+  if (user) {
+    localStorage.setItem("loggedUser", JSON.stringify(user));
+    window.location = "/index.html";
+  } else {
+    alert("Invalid email or password ❌");
   }
+
+  // Fetching the from the api(data base)
+  // try {
+  //   let usersRes = await fetch(api_user);
+  //   let users = await usersRes.json();
+
+  //   // Here finding user with match email and password
+  //   let user = users.find(
+  //     (u) => u.email === signInEmail && u.pass === signInPass
+  //   );
+  //   if (user) {
+  //     window.location = "/index.html";
+  //     // document.querySelector("#user_lastName").innerText = user.lName;
+  //   } else {
+  //     alert("Invalid email or password ❌");
+  //   }
+  // } catch (error) {
+  //   console.log("Server Error", error);
+  //   alert("Something went wrong! Please try again.");
+  // }
 
   document.querySelector(".signIn-form").reset();
 };
 
 let createAccBtn = document.querySelector(".create-account-btn");
-
-createAccBtn.addEventListener("click", () => {
-  window.location = "../HTML/Create_Account.html";
-});
+if(createAccBtn){
+  createAccBtn.addEventListener("click", () => {
+    window.location = "../HTML/Create_Account.html";
+  });
+}
