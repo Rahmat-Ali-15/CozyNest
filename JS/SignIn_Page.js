0f7af8ad6e278740eead6782d6e12b88f4042904
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-11-19 09:21:28 +0530

    Fetching data form the api to login

--- JS/SignIn_Page.js
+++ JS/SignIn_Page.js
@@ -1,27 +1,41 @@
-const defaultEmail = "rahmat786@gmail.com"
-const defaultPass = "rahmat@786"
+let api_user = "https://api-server-zecj.onrender.com/user";
 
-const signUpForm = (e) => {
+const signInForm = async (e) => {
   e.preventDefault();
 
-  let signUpEmail = document.querySelector("#email").value;
-  let signUpPass = document.querySelector("#password").value;
+  let signInEmail = document.querySelector("#email").value;
+  let signInPass = document.querySelector("#password").value;
 
-  if (signUpEmail.trim() === "" || signUpPass.trim() === "") {
+  if (signInEmail.trim() === "" || signInPass.trim() === "") {
     alert("All fields are required!!!");
     return false;
   }
 
-  if ((signUpEmail === defaultEmail) && (signUpPass === defaultPass)) {
-    alert("Login Successfull!!!");
+  // Fetching the from the api(data base)
+  try {
+    let usersRes = await fetch(api_user);
+    let users = await usersRes.json();
+
+    // Here finding user with match email and password
+    let user = users.find(
+      (u) => u.email === signInEmail && u.pass === signInPass
+    );
+    if (user) {
+      window.location = "/index.html";
+      // document.querySelector("#user_lastName").innerText = user.lName;
+    } else {
+      alert("Invalid email or password ❌");
+    }
+  } catch (error) {
+    console.log("Server Error", error);
+    alert("Something went wrong! Please try again.");
   }
-  else{
-    alert("Invalid email or Password !!!")
-  }
+
+  document.querySelector(".signIn-form").reset();
 };
 
 let createAccBtn = document.querySelector(".create-account-btn");
 
-createAccBtn.addEventListener("click", ()=> {
-    window.location = "../HTML/Create_Account.html"
+createAccBtn.addEventListener("click", () => {
+  window.location = "../HTML/Create_Account.html";
 });
