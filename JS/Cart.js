e8e25c66458f133fc40b25f0c4adaf2f3e8c669b
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-12-15 08:42:48 +0530

    Added logic for prices

--- JS/Cart.js
+++ JS/Cart.js
@@ -14,7 +14,7 @@ const increaseItem = () => {
   if (Number(itemInput.value) >= 10) {
     increaseBtn.disabled = true;
   }
-  handleAmount()
+  handleAmount();
 };
 const decreaseItem = () => {
   if (Number(itemInput.value) <= 0) {
@@ -29,75 +29,107 @@ const decreaseItem = () => {
   if (Number(itemInput.value) < 10) {
     increaseBtn.disabled = false;
   }
-  handleAmount()
+  handleAmount();
 };
 
 increaseBtn.addEventListener("click", increaseItem);
 decreaseBtn.addEventListener("click", decreaseItem);
 
-let itemPricePara = document.querySelector(".item-total-para");
-let itemRate = document.querySelector(".price");
-let itemQty = itemInput.value;
-let itemPrice = 1100;
-itemRate.innerText = "₹" + itemPrice;
-let totalItemPrice = Number(itemQty) * Number(itemPrice);
 const handleAmount = () => {
-
-  itemPricePara.innerText = "₹" + totalItemPrice.toLocaleString("en-IN", {
+  let itemPricePara = document.querySelector(".item-total-para");
+  let itemRate = document.querySelector(".price");
+  let itemQty = itemInput.value;
+  let itemPrice = 1100;
+  itemRate.innerText =
+    "₹" +
+    itemPrice.toLocaleString("en-IN", {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
-}
+  let totalItemPrice = Number(itemQty) * Number(itemPrice);
 
-const functions = () => {
+  itemPricePara.innerText =
+    "₹" +
+    totalItemPrice.toLocaleString("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  let merchandiseAmt =
+    "₹" +
+    totalItemPrice.toLocaleString("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  let shippingValue = (totalItemPrice * 2.5) / 100;
+  let shipping =
+    "₹" +
+    shippingValue.toLocaleString("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  let taxValue = (totalItemPrice * 28) / 100;
+  let tax =
+    "₹" +
+    taxValue.toLocaleString("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  let totalOrderAmt = totalItemPrice + shippingValue + taxValue;
+  let totalOrder =
+    "₹" +
+    totalOrderAmt.toLocaleString("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
   let cardOrderSummaryBox = document.querySelector(".cart-order-summary-box");
   cardOrderSummaryBox.innerHTML = `
     <div class="order-summary-heading">
-                        <i class="bi bi-bag-dash"></i>
-                        <h3 class="order-summary-title">Cart Summary</h3>
-                    </div>
-                    <div class="order-summary">
-                        <div class="item-price summary-box">
-                            <p>Merchandise:</p>
-                            <p name="amount">₹ ${itemPrice}.00</p>
-                        </div>
-                        <div class="shipping-details summary-box">
-                            <p>Shipping:</p>
-                            <p name="amount">₹100.00</p>
-                        </div>
-                        <div class="tax-details summary-box">
-                            <p>Tax:</p>
-                            <p name="amount">₹210.00</p>
-                        </div>
-                    </div>
-                    <div class="order-total">
-                        <p name="total-amount">Order Total:</p>
-                        <p name="total-amt">₹2410.00</p>
-                    </div>
-                    <div class="promocode-div">
-                        <p>Promo Code</p>
-                        <p>+</p>
-                    </div>
-                    <div class="checkout-btn">
-                        <div class="term-condition">
-                            <p>By checking out, you agree to our <a href="#">Terms of Use</a> and <a href="#">Privacy
-                                    Policy.</a></p>
-                        </div>
-                        <button>CHECKOUT NOW</button>
-                    </div>
-                    <div class="payment-method">
-                        <div class="phone-pay box">
-                            <p>PhonePay</p>
-                        </div>
-                        <div class="paytm box">
-                            <p>Paytm</p>
-                        </div>
-                    </div>
-  `
-}
+        <i class="bi bi-bag-dash"></i>
+        <h3 class="order-summary-title">Cart Summary</h3>
+    </div>
+    <div class="order-summary">
+        <div class="item-price summary-box">
+            <p>Merchandise:</p>
+            <p name="amount">${merchandiseAmt}</p>
+        </div>
+        <div class="shipping-details summary-box">
+            <p>Shipping:</p>
+            <p name="amount">${shipping}</p>
+        </div>
+        <div class="tax-details summary-box">
+            <p>Tax:</p>
+            <p name="amount">${tax}</p>
+        </div>
+    </div>
+    <div class="order-total">
+        <p name="total-amount">Order Total:</p>
+        <p name="total-amt">${totalOrder}</p>
+    </div>
+    <div class="promocode-div">
+        <p>Promo Code</p>
+        <p>+</p>
+    </div>
+    <div class="checkout-btn">
+        <div class="term-condition">
+            <p>By checking out, you agree to our <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a></p>
+        </div>
+        <button>CHECKOUT NOW</button>
+    </div>
+    <div class="payment-method">
+        <div class="phone-pay box">
+            <p>PhonePay</p>
+        </div>
+        <div class="paytm box">
+            <p>Paytm</p>
+        </div>
+    </div>
+  `;
+};
 
 window.onload = () => {
-  decreaseItem();
   handleAmount();
-  functions()
-}
+};
